30c52e79fb76e102c6b2889e6cfb3c70ae5e1c8e
Himani <himanih023@gmail.com>
2023-07-21 10:27:26 +0530

    cart interaction

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,6 +73,11 @@ let matchingItem;
     });
 
   }
+  let cartQuantity = 0;
+  cart.forEach((item) =>{
+    cartQuantity += item.quantity;
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   
 });
 });
